e829dd8c9a002cfe5db1ab3115e9ab1379281932
nlok5923 <nlokhande5923@gmail.com>
2021-06-08 20:54:23 +0530

    added frontend code for login and signup components

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Button, Checkbox, Form } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
+
+const LoginForm = () => (
+  <Form>
+    <Form.Field>
+      <label>First Name</label>
+      <input placeholder='First Name' />
+    </Form.Field>
+    <Form.Field>
+      <label>Last Name</label>
+      <input placeholder='Last Name' />
+    </Form.Field>
+    <Form.Field>
+      <Checkbox label='I agree to the Terms and Conditions' />
+    </Form.Field>
+    <Button type='submit'>Submit</Button>
+  </Form>
+)
+
+export default LoginForm;
---
+++ src/pages/Signup/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Button, Checkbox, Form } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
+
+const SignupForm = () => (
+  <Form>
+    <Form.Field>
+      <label>First Name</label>
+      <input placeholder='First Name' />
+    </Form.Field>
+    <Form.Field>
+      <label>Last Name</label>
+      <input placeholder='Last Name' />
+    </Form.Field>
+    <Form.Field>
+      <label>First Name</label>
+      <input placeholder='First Name' />
+    </Form.Field>
+    <Form.Field>
+      <label>Last Name</label>
+      <input placeholder='Last Name' />
+    </Form.Field>
+    <Form.Field>
+      <label>First Name</label>
+      <input placeholder='First Name' />
+    </Form.Field>
+    <Form.Field>
+      <label>Last Name</label>
+      <input placeholder='Last Name' />
+    </Form.Field>
+    <Button type='submit'>Submit</Button>
+  </Form>
+)
+
+export default SignupForm;
