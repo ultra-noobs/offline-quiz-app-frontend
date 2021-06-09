eeadf218633799297c75634b26f2cd31d712a9a2
Keshav Agarwal <keshavagarwal1710@gmail.com>
2021-06-09 11:57:54 +0530

    Accepting New Change

--- src/components/nav/navbar.jsx
+++ src/components/nav/navbar.jsx
@@ -2,13 +2,13 @@ import "./navbar.scss";
 import {Link} from 'react-router-dom';
 const Navbar = ()=>{
     return(
-        <nav>
+        <nav className="navbar">
             <div className="logo">
                 OffQuiz
             </div>
             <ul className="navLink">
-                <li><Link to="/">Login</Link></li>
-                <li><Link to="/">SignUp</Link></li>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/register">SignUp</Link></li>
                 <li><Link to="/">About Us</Link></li>
             </ul>
         </nav>
